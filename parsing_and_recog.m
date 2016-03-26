4f84f28abb407a15a50131c5dac45d3f0262821f
snowm <example@example.com>
2016-03-26 10:48:27 +0100

    update parsing_and_recog.m

--- parsing_and_recog.m
+++ parsing_and_recog.m
@@ -39,7 +39,7 @@ if correct =='1'
    % set the output annotation file name	
 	outID = fopen(fullfile(img_annotation,img_name,sprintf('%s_correct_recog.annotation',img_name)),'w');
 else
-	%read the raw segmentation file directly
+	gread the raw segmentation file directly
 	fileID = fopen(fullfile(img_annotation,img_name,sprintf('%s_seg.annotation',img_name)),'r');
 	outID = fopen(fullfile(img_annotation,img_name,sprintf('%s_recog.annotation',img_name)),'w');
 end
@@ -50,7 +50,7 @@ else
     gray = im;
 end
 
-%load the Thickness parameter
+%load the lThickness/lSpace parameter
 path_DPfile = fullfile(img_annotation,img_name,'DP_log');
 		
 [status result] = system(['tail -n 1 ' path_DPfile]); %extract the last line of DP_log
